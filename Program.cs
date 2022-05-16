5ebd62a38dab0d08a59b27a1b2bd8f2c843d14d0
Uxeruses <jakubbrzoskowski@gmail.com>
2022-05-16 23:31:05 +0200

    Basic word query works

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SongGenerator
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var generator = new RandomWordClient();
+            var results = await generator.GetRandomWord(5);
+            foreach (var result in results)
+            {
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
---
+++ RandomWordClient.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SongGenerator
+{
+    class RandomWordClient
+    {
+        private readonly HttpClient client;
+        public RandomWordClient()
+        {
+            client = new HttpClient();
+        }
+
+        public async Task<string[]> GetRandomWord(int n)
+        {
+            Task<string>[] taskList = new Task<string>[n];
+            for(int i = 0; i < n; i++)
+            {
+                taskList[i] = client.GetStringAsync("https://random-words-api.vercel.app/word");
+            }
+            var results = await Task.WhenAll(taskList);
+            ExtractOnlyWord(ref results);
+            return results;
+        }
+        private void ExtractOnlyWord(ref string[] rawData)
+        {
+            var control = new char[] { '[', ']' };
+            for (int i = 0; i < rawData.Length; i++)
+            {
+                rawData[i] = rawData[i].Trim(control);
+                var dict = JsonSerializer.Deserialize<Dictionary<string, string>>(rawData[i]);
+                rawData[i] = dict["word"];
+            }
+        }
+    }
+}
